a348279900ad5da715215bbe022c2662468007dc
AVWORLD\rob9575 <rsantos@esri.com>
2021-04-29 16:37:51 -0500

    code cleanup, change icon, remove default styling, project defaults

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 import LinkedMapsContainer from './LinkedMaps/LinkedMapsContainer';
 
 function App() {
   return (
-    <div className="App">
+    <div id='rootDiv'>
         <LinkedMapsContainer></LinkedMapsContainer>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Checks basic render of page', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = document.getElementById('rootDiv');
   expect(linkElement).toBeInTheDocument();
 });
