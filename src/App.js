93c687eb5c427483be52462dacbe4601b7d92cd4
AVWORLD\rob9575 <rsantos@esri.com>
2021-04-30 13:47:02 -0500

    map and scene

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useRef, useEffect } from "react";
 import MapView from "@arcgis/core/views/MapView";
-import WebMap from "@arcgis/core/WebMap";
 import SceneView from '@arcgis/core/views/SceneView';
 import Map from '@arcgis/core/Map';
 
@@ -10,30 +9,38 @@ import "./App.css";
 function App() {
 
   const mapDiv = useRef(null);
-  // const sceneDiv = useRef(null);
+  const sceneDiv = useRef(null);
+  var map = new Map({
+    basemap: "satellite"
+  });
 
   useEffect(() => {
     if (mapDiv.current) {
       /**
        * Initialize application
        */
-       var map = new Map({
-        basemap: "satellite"
-      });
 
       const view = new MapView({
         container: mapDiv.current,
         map: map
       });
-
-      // const sceneView = new SceneView({
-      //   container: sceneDiv.current,
-      //   map: webmap
-      // });
     }
   }, []);
 
-  return <div className="mapDiv" ref={mapDiv}></div>;
+  useEffect(() => {
+    if (sceneDiv.current) {
+
+      const sceneview = new SceneView({
+        container: sceneDiv.current,
+        map: map
+      });
+    }
+  }, []);
+
+  return <>
+      <div className="mapDiv" ref={mapDiv}></div>
+      <div className="sceneDiv" ref={sceneDiv}></div>
+    </>;
 }
 
 export default App;
